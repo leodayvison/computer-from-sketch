169b9658a8c7d3f62c534cafe0f6e50b40fed5ba
leodayvison <leonardodayvison235@gmail.com>
2025-10-26 12:47:07 -0300

    minor changes

--- design/sum.vhd
+++ design/sum.vhd
@@ -13,26 +13,23 @@ entity sum is
 end sum;
 
 architecture sumARCH of sum is
-    -- O 'carry' não precisa ser um sinal (signal) aqui
 begin
     process(a, b, cin)
-        -- Declare o carry como uma VARIÁVEL dentro do process
         variable carry_v: std_logic_vector(8 downto 0);
     begin
-        -- 1. Use atribuição de variável (:=)
         carry_v(0) := cin;
 
-        -- 2. Loop para o ripple-carry
+        
         for n in 0 to 7 loop
-            -- A saída 'f' (um sinal) ainda usa '<='
+        
             f(n) <= carry_v(n) xor a(n) xor b(n);
             
-            -- O 'carry_v' (uma variável) usa ':='
-            -- Esta atualização acontece IMEDIATAMENTE.
+        
+        
             carry_v(n+1) := (a(n) and b(n)) or (carry_v(n) and (a(n) xor b(n)));
         end loop;
 
-        -- 3. A saída 'cout' (sinal) recebe o valor final da variável
+        
         cout <= carry_v(8);
     end process;
 end sumARCH;
