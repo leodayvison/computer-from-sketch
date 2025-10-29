33cb653562c59493811421f86898b911e3ef1c22
leodayvison <leonardodayvison235@gmail.com>
2025-10-29 19:56:58 -0300

    Implemented REGFILE read/write architecture

--- design/basicProcesser.vhdl
+++ design/basicProcesser.vhdl
@@ -22,26 +22,45 @@ end controlDecoderEntity;
 
 
 
--------Bank Register Entity:
+--------------- REGISTOR BANK ---------------
 
-entity registerBankEntity is
+entity regfile is
 port(
-    clk: IN std_logic;
-    reset: IN std_logic; --Reset the register BANK
-    WE: IN std_logic; --Choose the operator (write/read)
-    address: IN unsigned(2 downto 0); --Choose the register
-    data: INOUT std_logic_vector(7 downto 0);
+    clk   : IN std_logic;
+    reset : IN std_logic; --Reset the register BANK
+    we    : IN std_logic; --Choose the operator (write/read)
+    addr  : IN unsigned(2 downto 0); --Choose the register
+    data  : INOUT std_logic_vector(7 downto 0);
     
-    r0: INOUT std_logic_vector(7 downto 0); -- endereco 000
-    r1: INOUT std_logic_vector(7 downto 0); -- endereco 001
-    r2: INOUT std_logic_vector(7 downto 0); -- endereco 010
-    r3: INOUT std_logic_vector(7 downto 0); -- endereco 011
-    flags: INOUT std_logic_vector(2 downto 0) -- endereco 100
+    r0    : INOUT std_logic_vector(7 downto 0); -- endereco 000
+    r1    : INOUT std_logic_vector(7 downto 0); -- endereco 001
+    r2    : INOUT std_logic_vector(7 downto 0); -- endereco 010
+    r3    : INOUT std_logic_vector(7 downto 0); -- endereco 011
+    flags : INOUT std_logic_vector(2 downto 0) -- endereco 100
     -- FLAGS: zero (2), overflow (1), carry (0)
     );
-end registerBankEntity;
+end regfile;
 
+architecture reg of regfile is
+begin
 
+    type reg_array is array(0 to 3) of std_logic_vector(7 downto 0); -- vetor (banco) de vetores (registradores)
+    signal regs  : reg_array := (others=>(others=>'0')); -- zera todos os registradores
+
+    process(clk) -- escrita, reset
+    begin
+        if rising_edge(clk) then
+            if rst = '1' then 
+                regs <= (others=>(others=>'0'));
+            elsif we = '1' then
+                regs(to_integer(addr)) <= data; 
+            end if;
+        end if;
+    end process;
+
+    data <= regs(to_integer(addr)) when we = '0' else (others => 'Z');
+
+end reg;
 
 
 ---------------------------------------------------------
@@ -62,13 +81,13 @@ signal state    : stateType := loadOpcode;
 --ALU component:
 component ulaEntity is
 port(
-    a   : in  std_logic_vector(7 downto 0);
-    b   : in  std_logic_vector(7 downto 0);
-    f   : out std_logic_vector(7 downto 0);
-    s   : in  std_logic_vector(2 downto 0);
-    cout: out std_logic;
-    en  : in  std_logic;
-    clk : in  std_logic
+    a    : in  std_logic_vector(7 downto 0);
+    b    : in  std_logic_vector(7 downto 0);
+    f    : out std_logic_vector(7 downto 0);
+    s    : in  std_logic_vector(2 downto 0);
+    cout : out std_logic;
+    en   : in  std_logic;
+    clk  : in  std_logic
     );
 end component;
 
@@ -155,6 +174,9 @@ begin
 	process(ULAoutput, ULAenable)
     begin
         output <= ULAoutput;
+        if ULAoutput = "00000000" then 
+
+            
     end process;
 
 
